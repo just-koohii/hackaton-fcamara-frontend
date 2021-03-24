9a9929923d9561b8766a36005b0616b71a945a1f
Matheus Ferreira <math.ferr@outlook.com>
2021-03-24 10:02:15 -0300

    Correção do nome das classes tela2 e tela3

--- pages/tela2.tsx
+++ pages/tela2.tsx
@@ -2,7 +2,7 @@ import { PureComponent } from "react";
 import { Heading } from "@Components";
 import styles from "../styles/Tela.module.css";
 
-export default class Tela1 extends PureComponent {
+export default class Tela2 extends PureComponent {
   render() {
     return (
       <div className={`${styles.root} ${styles.tela02}`}>
