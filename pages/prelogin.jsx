e3a15f5e84efd760e283991051079238661745b6
Daiane <daianesantana_f@hotmail.com>
2021-04-01 12:06:18 -0300

    tela do doador

--- pages/prelogin.jsx
+++ pages/prelogin.jsx
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ContainedButtons() {
+export default function preLogin() {
   const classes = useStyles();
 
   return (
