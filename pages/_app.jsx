9142de13e3682c1d52342f627d4b81c9e74ad3d4
Matheus Ferreira <math.ferr@outlook.com>
2021-04-02 15:45:59 -0300

    Correção da navbar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import App from 'next/app';
+import { useEffect } from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
@@ -6,32 +6,35 @@ import { NavBar } from '@components';
 import theme from '../src/theme';
 import '../styles/globals.css';
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-    return (
-      <>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-          <meta name="theme-color" content={theme.palette.primary.main} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content={theme.palette.primary.main} />
 
-          <title>Elo</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <title>Elo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <NavBar />
-          <main>
-            <Component {...pageProps} />
-          </main>
-        </ThemeProvider>
-      </>
-    );
-  }
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </>
+  );
 }
